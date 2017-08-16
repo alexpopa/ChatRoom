235a7b578920b94492c22ce08c85fb2ec3b36623
Alexandru Popa <apopa@cablevision.com>
2017-08-16 14:25:11 -0400

    setting up server.js

---
+++ Server/server.js
@@ -0,0 +1,8 @@
+// server.js
+
+// Import all our dependencies
+var express  = require('express');
+var mongoose = require('mongoose');
+var app      = express();
+var server   = require('http').Server(app);
+var io       = require('socket.io')(server);
